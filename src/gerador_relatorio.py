4b19f49adeb1e6bc3d66798942af6998f51b8905
Mardony Lima <Mardony.lima.cj12@gmail.com>
2025-05-07 22:29:50 -0300

    gerador_relatorio completed

---
+++ src/gerador_relatorio.py
@@ -0,0 +1,25 @@
+import os
+import csv
+
+from src.coleta_sistema import get_So_name, get_cpu_percent, get_mem_total, get_disc_usage
+from src.coleta_rede import get_mac_address, get_ip_address
+
+
+def creat_file():
+    with open('./relatorio_teste.csv', mode='w', newline='', encoding='latin-1') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        writer.writerow(['Sistema Operacional:', get_So_name()])
+        writer.writerow(['Uso de CPU:', get_cpu_percent()])
+        writer.writerow(['Memoria Total:', get_mem_total()])
+        writer.writerow(['Memoria Usanda:', get_disc_usage()])
+        writer.writerow(['Endereço IP:', get_ip_address()])
+        writer.writerow(['MAC da Maquina', get_mac_address()])
+
+
+def clean_report():
+    path = './relatorio_teste.csv'
+    if os.path.exists(path):
+        os.remove(path)
+        print(f"O arquivo '{path}' foi removido com sucesso!")
+    else:
+        print(f"O arquivo '{path}' não existe!")
